16c51399d84eb521e54471c5b06524c5843e6d5a
Gunthee <sirarat.t@ku.th>
2019-08-22 10:43:01 +0700

    done

--- src/test/java/th/ku/emailtemplate/TemplateTest.java
+++ src/test/java/th/ku/emailtemplate/TemplateTest.java
@@ -4,11 +4,52 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class TemplateTest {
-
+	
+	private Template template;
+	
+	@BeforeEach
+	void setup() {
+	template = new Template("${one}, ${two}, ${three}");
+	template.set("one", "1");
+	template.set("two", "2");
+	template.set("three", "3");
+	}
+    
     @Test
-    public void testEvaluateOneVariable() {
-
+	void testEvaluateOneVariable() {
+		Template template = new Template("Hello, ${name}");
+		template.set("name", "Reader");
+		assertEquals("Hello, Reader", template.evaluate());
+	}
+    
+    @Test
+    void testEvaluateDifferentValue() {
+	    Template template = new Template("Hi, ${name}");
+	    template.set("name", "someone else");
+	    assertEquals("Hi, someone else", template.evaluate());
     }
 
+    @Test
+	void testEvalueMultipleVariables() {
+		assertTemplateEvaluatesTo("1, 2, 3");
+	}
+	
+	@Test
+	void testEvaluateUnknownVariablesAreIgnored() {
+		template.set("doesnotexist", "whatever");
+		assertTemplateEvaluatesTo("1, 2, 3");
+	}
+	
+	private void assertTemplateEvaluatesTo(String expected) {
+		assertEquals(expected, template.evaluate());
+	}
+	
+	@Test
+	public void testEvaluateMissingValueRaisesException() {
+		Throwable exception = assertThrows(MissingValueException.class,() -> { new Template("${foo}").evaluate(); });
+		assertEquals("No value for ${foo}", exception.getMessage());
+	}
 }
