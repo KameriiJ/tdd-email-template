10d2bcd8b409e5e11c3246d32a3eafeb9d249551
Gunthee <sirarat.t@ku.th>
2019-08-22 21:10:19 +0700

    😁 test emoji

--- src/main/java/th/ku/emailtemplate/Template.java
+++ src/main/java/th/ku/emailtemplate/Template.java
@@ -7,27 +7,25 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Template {
-	
+
 	private Map<String, String> variables;
 	private String templateText;
-	
-	public Template(String templateText) 
-	{
+
+	public Template(String templateText) {
 		this.variables = new HashMap<String, String>();
 		this.templateText = templateText;
 	}
-	
-	public void set(String variable, String value) 
-	{
+
+	public void set(String variable, String value) {
 		this.variables.put(variable, value);
 	}
-	
+
 	public String evaluate() {
 		String result = replaceVariables();
 		checkForMissingValues(result);
 		return result;
 	}
-	
+
 	private String replaceVariables() {
 		String result = templateText;
 		for (Entry<String, String> entry : variables.entrySet()) {
@@ -36,9 +34,11 @@ public class Template {
 		}
 		return result;
 	}
-	
+
 	private void checkForMissingValues(String result) {
 		Matcher m = Pattern.compile(".*\\$\\{.+\\}.*").matcher(result);
-		if (m.find()) throw new MissingValueException("No value for " + m.group());
+		if (m.find())
+			throw new MissingValueException("No value for " + m.group());
 	}
+	// test
 }
